084793aac8610d8bf183a47f2836f02f6d92d7ab
Marcel Weiher <marcel@metaobject.com>
2019-10-29 19:20:21 +0100

    properly recognize all the load commands in an a.out and a small framework

--- ObjectiveTcc/MPWMachOFile.m
+++ ObjectiveTcc/MPWMachOFile.m
@@ -29,7 +29,8 @@
     const struct load_command *command=[machofile bytes] + sizeof *header;
     NSMutableArray *segments=[NSMutableArray array];
     for (int i=0;i<numCommands;i++) {
-        switch ( command->cmd) {
+        int cmd = command->cmd;
+        switch ( cmd) {
             case LC_SEGMENT_64:
             {
                 printf("segment 64 length %d\n",command->cmdsize);
@@ -38,8 +39,93 @@
                 [segments addObject:segment];
                 break;
             }
+            case LC_SYMTAB:
+            {
+                printf("symtab length %d\n",command->cmdsize);
+                break;
+            }
+
+            case LC_DYSYMTAB:
+            {
+                printf("dsymtab length %d\n",command->cmdsize);
+                break;
+            }
+            case LC_LOAD_DYLINKER:
+            {
+                printf("load dynamic linker length %d\n",command->cmdsize);
+                break;
+            }
+            case LC_UUID:
+            {
+                printf("UUID length %d\n",command->cmdsize);
+                break;
+            }
+
+            case LC_LOAD_DYLIB:
+            {
+                struct dylib_command *dylib=(struct dylib_command*)command;
+                char *name=(char*)command;
+                int offset=dylib->dylib.name.offset;
+                int nameLen=command->cmdsize - offset;
+                name+=offset;
+
+                printf("dynamic library: %*s\n",nameLen,name);
+                break;
+            }
+            case LC_DYLD_INFO_ONLY:
+            case LC_DYLD_INFO:
+            {
+                printf("dyld info (compressed) library length %d\n",command->cmdsize);
+                break;
+            }
+            case LC_MAIN:
+            {
+                printf("LC_MAIN length %d\n",command->cmdsize);
+                break;
+            }
+
+            case LC_DATA_IN_CODE:
+            {
+                printf("data in code %d\n",command->cmdsize);
+                break;
+            }
+            case LC_SOURCE_VERSION:
+            {
+                printf("LC_SOURCE_VERSION length %d\n",command->cmdsize);
+                break;
+            }
+
+            case LC_FUNCTION_STARTS:
+            {
+                printf("LC_FUNCTION_STARTS %d\n",command->cmdsize);
+                break;
+            }
+            case LC_BUILD_VERSION: {
+                 printf("LC_BUILD_VERSION %d\n",command->cmdsize);
+                 break;
+             }
+            case LC_ID_DYLIB:
+            {
+                printf("LC_ID_DYLIB %d\n",command->cmdsize);
+                break;
+            }
+
+            case LC_CODE_SIGNATURE:
+            {
+                printf("LC_CODE_SIGNATURE %d\n",command->cmdsize);
+                break;
+            }
+            case LC_VERSION_MIN_MACOSX:
+            {
+                printf("LC_VERSION_MIN_MACOSX %d\n",command->cmdsize);
+                break;
+            }
+
+
+
+
             default:
-                printf("command type %x length %d\n",command->cmd,command->cmdsize);
+                printf("======= command type %x length %d\n",cmd,command->cmdsize);
         }
         command = (struct load_command*)(((char*)command)+command->cmdsize);
     }
