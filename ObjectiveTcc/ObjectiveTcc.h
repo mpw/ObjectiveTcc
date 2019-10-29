50af6ce0bb9f7e39b43af585787c9ec3367f5e84
Marcel Weiher <marcel@metaobject.com>
2019-10-29 13:33:28 +0100

    Initial Commit

---
+++ ObjectiveTcc/ObjectiveTcc.h
@@ -0,0 +1,19 @@
+//
+//  ObjectiveTcc.h
+//  ObjectiveTcc
+//
+//  Created by Marcel Weiher on 29.10.19.
+//  Copyright © 2019 metaobject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjectiveTcc.
+FOUNDATION_EXPORT double ObjectiveTccVersionNumber;
+
+//! Project version string for ObjectiveTcc.
+FOUNDATION_EXPORT const unsigned char ObjectiveTccVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveTcc/PublicHeader.h>
+
+
